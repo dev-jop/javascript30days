9880a3c288d00515373ce74c6a72acc26fc09061
jop <joefrey.ramos@bibo.com.ph>
2017-05-11 10:53:31 +0800

    update

--- drum.js
+++ drum.js
@@ -2,11 +2,13 @@ var music = {
     init: function(e) {
         var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
         var key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        var bgMusic = document.getElementById('bg-music');
         if (!audio) return;
         if (e.keyCode == 69) {
             document.querySelector('#key-e').classList.add('playing');
         }
         audio.currentTime = 0;
+        this.playBgMusic(bgMusic);
         audio.play();
         key.classList.add('playing');
         var keys = document.querySelectorAll('.key');
@@ -15,6 +17,9 @@ var music = {
     removeTransistion: function(e) {
         if (e.propertyName != 'transform') return;
         this.classList.remove('playing');
+    },
+    playBgMusic: function(bgMusic) {
+        bgMusic.play();
     }
 };
 
